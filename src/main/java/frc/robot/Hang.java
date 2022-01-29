69b1bc454b6a77b9f59a3eab2423a6a847eb9ca1
Wincy Perez <wincyperez@gmail.com>
2022-01-28 18:00:53 -1000

    Created constructor for the hang class and started on enumerations.

--- src/main/java/frc/robot/Hang.java
+++ src/main/java/frc/robot/Hang.java
@@ -8,14 +8,46 @@ import com.kauailabs.navx.frc.AHRS;
 
 public class Hang {
     
+    /////////////////////////////////////////////
+    //                                         //
+    //                VARIABLES                //
+    //                                         //
+    ///////////////////////////////////////////// 
+
+    //ELEVATOR MOTOR
     private MotorController elevatorMotor;
+    private TalonFXSensorCollection elevatorEncoder;
     private DigitalInput limitTop;                  
     private DigitalInput limitBot;
     
+    //PIVOT MOTOR
     private MotorController pivotMotor;
     private TalonEncoder pivotEncoder; 
     private DigitalInput frontLimit;   
     private DigitalInput backLimit;
+    private AHRS navX;
+
+    /////////////////////////////////////////////
+    //                                         //
+    //              CONSTRUCTORS               //
+    //                                         //
+    /////////////////////////////////////////////
+
+    public Hang(MotorController elevMotor, DigitalInput limitSwitchTop, DigitalInput limitSwitchBottom, TalonFXSensorCollection elevEncoder, MotorController hangPivotMotor, TalonEncoder hangPivotEncoder, AHRS gyro, DigitalInput frontLimitSwitch, DigitalInput backLimitSwitch ){
+        elevatorMotor = elevMotor;
+        limitTop = limitSwitchTop;
+        limitBot = limitSwitchBottom;
+        elevatorEncoder = elevEncoder;
+        pivotMotor = hangPivotMotor;
+        pivotEncoder = hangPivotEncoder;
+        frontLimit = frontLimitSwitch;
+        backLimit = backLimitSwitch;
+        navX = gyro;
+    }
+
+    private enum hangStates{
+
+    }
 
     
 }
